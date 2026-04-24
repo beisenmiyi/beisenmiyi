11440c6398c61802bea761af2133c2cc0b8f1025
beisenmiyi <beisenmiyi@qq.com>
2026-04-25 00:02:44 +0800

    添加client中app-（header、main、footer）组件

---
+++ client/app/components/main/Main.tsx
@@ -0,0 +1,9 @@
+import Welcome from "./welcome/welcome";
+
+export default function Main() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Welcome />
+    </div>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import Welcome from "./components/welcome/welcome";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Welcome />
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
